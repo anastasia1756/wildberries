cc4974749ee3dd6f40c30aec862dcf682e26ee01
anastasia1756 <nastya7@ukr.net>
2021-11-22 00:00:18 +0300

    render cards

--- js/getGoods.js
+++ js/getGoods.js
@@ -3,7 +3,29 @@ const getGoods = () => {
 
     const renderGoods = (goods) => {
     const goodsContainer = document.querySelector('.long-goods-list');
-console.log(goodsContainer);
+
+    goodsContainer.innerHTML = '';
+
+
+    goods.forEach(good => {
+        const goodBlock = document.createElement('div');
+
+        goodBlock.classList.add('col-lg-3');
+        goodBlock.classList.add('col-sm-6');
+
+        goodBlock.innerHTML = `
+            <div class="goods-card">
+				<span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+				<img src="db/${good.img}" alt="${good.name}" class="goods-image">
+				<h3 class="goods-title">${good.name}</h3>
+				<p class="goods-description">${good.description}</p>
+				<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+				    <span class="button-price">$${good.price}</span>
+				</button>
+			</div>
+        `
+        goodsContainer.append(goodBlock);
+    })
     }
 
     const getData = (value, category) => {
